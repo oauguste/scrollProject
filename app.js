81d9d09974acf070e3cc96dfd65d76fc90695e2d
oauguste <augusteosnac@gmail.com>
2023-03-08 09:53:09 -0500

    Html set-Up

---
+++ app.js
@@ -0,0 +1,11 @@
+//set Date
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+//Close links **************
+
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(
+  ".links-container"
+);
+const links = document.querySelector(".links");
